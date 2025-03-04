71e8df9949166769acd09bf5a880ef8baeac1d13
yuzhengyang <inc@live.cn>
2025-03-04 10:44:26 +0800

    test vue page

---
+++ config.js
@@ -0,0 +1,13 @@
+var configs = [
+    {
+        host: '*',
+        author: '*',
+        environment: '默认配置',
+        baseServer: 'http://192.168.14.155/hidoc_server_api/',
+        imageServer: 'http://192.168.14.155/hidoc_server_api/f/d/u/',
+        hdImageFlag: '#hd.image->',
+        hdIlinkFlag: '#hd.ilink->',
+        networkType: 'inner',
+        mastLogin: true
+    }
+];
